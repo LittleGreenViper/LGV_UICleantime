9ddfc40f8b0a7cbbccb557f07e8aadb20f0b214a
Chris Marshall <chris@riftvalleysoftware.com>
2025-02-07 09:21:48 -0500

    OK. We now have a separate keytag image generator class.

--- Sources/LGV_UICleantime/Images/Keytags/LGV_UISingleCleantimeKeytagImageView.swift
+++ Sources/LGV_UICleantime/Images/Keytags/LGV_UISingleCleantimeKeytagImageView.swift
@@ -20,10 +20,10 @@
  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#if os(iOS) // We don't want this around, if we will be using it in non-IOS contexts.
 import LGV_Cleantime
 import UIKit
 
+#if os(iOS) // We don't want this around, if we will be using it in non-IOS contexts.
 /* ###################################################################################################################################### */
 // MARK: - Single Cleantime Keytag Display View -
 /* ###################################################################################################################################### */
@@ -101,6 +101,72 @@ open class LGV_UISingleCleantimeKeytagImageView: LGV_UICleantimeImageViewBase {
      This needs to be implemented in the main class declaration.
      */
     public override var generatedImage: UIImage? {
+        LGV_KeytagImageGenerator(isRingClosed: isRingClosed, totalDays: totalDays, totalMonths: totalMonths).generatedImage
+    }
+}
+#endif
+
+/* ###################################################################################################################################### */
+// MARK: - Single Cleantime Keytag Image Generator -
+/* ###################################################################################################################################### */
+/**
+ This is specified as a separate class, so it can be used by the Watch.
+ */
+class LGV_KeytagImageGenerator {
+    /* ################################################################################################################################## */
+    // MARK: - Cleantime Keytag Ring Resource Names -
+    /* ################################################################################################################################## */
+    /**
+     This enum has the names for all of the various keytag ring visual resources.
+     It is string, because we use the values to get images from the bundle.
+     */
+    public enum KeytagResourceNamesRing: String {
+        /* ########################################################## */
+        /**
+         This should not happen. It's an error.
+         */
+        case none = ""
+
+        /* ########################################################## */
+        /**
+         This means that the ring is "closed" at the top.
+         */
+        case ring_Closed
+
+        /* ########################################################## */
+        /**
+         This means that the ring is "open" at the top.
+         */
+        case ring_Open
+    }
+
+    /* ################################################################## */
+    /**
+     True (default) if the ring is closed on top.
+     If it is changed, the cached image is deleted, forcing a recalculation.
+     */
+    var isRingClosed: Bool = true
+    
+    /* ################################################################## */
+    /**
+     The total number of days to display.
+     If it is changed, the cached image is deleted, forcing a recalculation.
+     */
+    var totalDays: Int = 0
+    
+    /* ################################################################## */
+    /**
+     The total number of months to display (including years).
+     If it is changed, the cached image is deleted, forcing a recalculation.
+     */
+    var totalMonths: Int = 0
+
+    /* ################################################################## */
+    /**
+     This returns a dynamically-generated keytag image.
+     This needs to be implemented in the main class declaration.
+     */
+    var generatedImage: UIImage? {
         guard let keyTagDescription = LGV_CleantimeKeytagDescription.getLastTagThatApplies(totalDays: totalDays, totalMonths: totalMonths),
            let bodyImage = UIImage(named: keyTagDescription.bodyImage.rawValue),
            let textImage = UIImage(named: keyTagDescription.textImage.rawValue),
@@ -115,8 +181,21 @@ open class LGV_UISingleCleantimeKeytagImageView: LGV_UICleantimeImageViewBase {
         defer { UIGraphicsEndImageContext() }
         ringImage.draw(in: boundRect, blendMode: .normal, alpha: 1)
         bodyImage.draw(in: boundRect, blendMode: .normal, alpha: 1)
-        textImage.draw(in: boundRect, blendMode: .normal, alpha: 1)        
+        textImage.draw(in: boundRect, blendMode: .normal, alpha: 1)
         return UIGraphicsGetImageFromCurrentImageContext()
     }
+    
+    /* ################################################################## */
+    /**
+     Main initializer.
+     
+     - parameter isRingClosed: True, if the ring is "closed."
+     - parameter totalDays: The total number of days, represented by the keytag.
+     - parameter totalMonths: The total number of months, represented by the keytag.
+     */
+    init(isRingClosed inIsRingClosed: Bool, totalDays inTotalDays: Int, totalMonths inTotalMonths: Int) {
+        isRingClosed = inIsRingClosed
+        totalDays = inTotalDays
+        totalMonths = inTotalMonths
+    }
 }
-#endif
